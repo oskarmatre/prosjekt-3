265a17eeb02180a74a940bd6c3332a37e900d547
Sara Holm Pedersen <saraped99@gmail.com>
2022-10-11 10:54:43 +0200

    paragraph error test

--- assignment3.py
+++ assignment3.py
@@ -1,27 +1,38 @@
 import random
 import string
 import codecs
+
 random.seed(123) 
 f = codecs.open("pg3300.txt", "r", "utf-8")
 par = f.read()
 f.close()
-par = par.split(' \r\n')
-#print(par[0])
+par = par.split(' \n')
+print(par[14])
 
 def gut(p):
     return "gutenberg" not in p.lower()
+
 def tokenize(p):
     s = string.punctuation+"\n\r\t"
     for c in s:
-        print(c)
+        # print(c)
         p.replace(c,"")
     return p
+
 filtered_par = list(filter(gut, par))
-filtered_par = list(map(lambda p: tokenize(p), filtered_par))
-print(filtered_par[49])
+
+# filtered_par = list(map(lambda p: tokenize(p), filtered_par)) # litt usikker på om vi trenger denne
+# print(filtered_par[4])
+
+def empty(p):
+    return len(p) > 0 
 
 tokens = list(map(lambda p: p.split(" "), filtered_par))
-print(tokens[49])
+# for item in tokens :
+#     print(item) 
+#     item = list(filter(empty, item))
+
+# print(tokens[0])
 
 # table = p.maketrans(string.punctuation+"\n\r\t","!"*35)
 # p =p.translate(table)
